c83441ea44b306370c22e0268b5775432d454ef5
hernandezjosze <josehdzg097@gmail.com>
2025-03-05 18:34:09 -0600

    FrontEnd
    Added request to webserver for chatbot added
    Improved chat box logic

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -1,23 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
   const user = "Usuario"
+  const chatbot = "Chatbot"
   const chat_msgs = document.getElementById("chat-messages");
   const input_msgs = document.getElementById("message-input");
+  const conversation_id = document.getElementById("conversation_id_input");
 
   function AppendToChat(sender, msg) {
-    if(input_msgs.value === "") {
+    if(msg === "") {
       return;
     }
 
     let new_input_msg = document.createElement("div");
-    new_input_msg.textContent = sender + ": " + msg.value;
+    new_input_msg.textContent = sender + ": " + msg;
     chat_msgs.appendChild(new_input_msg);
-    input_msgs.value = "";
+    chat_msgs.appendChild(document.createElement("br"));
     chat_msgs.scrollTop = chat_msgs.scrollHeight;
   };
 
   const enter_btn = document.getElementById("send-button");
-  enter_btn.onclick = () => {
-    AppendToChat(user, input_msgs);
+  enter_btn.onclick = async () => {
+    const input = input_msgs.value;
+    input_msgs.value = "";
+    AppendToChat(user, input);
+
+    // Define the API URL
+    const apiUrl = 'http://0.0.0.0:8000/chatbot?id=' + conversation_id.value + '&message=' + input;
+    // Make a GET request
+    let chatbot_res = "";
+    await fetch(apiUrl)
+        .then(response => {
+          if (!response.ok) {
+            console.log(response);
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(data => {
+          chatbot_res = data;
+          console.log(data);
+        })
+        .catch(error => {
+          console.error('Got an error:', error);
+        });
+    for(const json of chatbot_res["message"]) {
+      console.log(json["content"]);
+    }
+    AppendToChat(chatbot, chatbot_res["message"].at(-1)["content"]);
+    conversation_id.value = chatbot_res["conversation_id"];
   }
 
   input_msgs.addEventListener("keypress", (e) => {
